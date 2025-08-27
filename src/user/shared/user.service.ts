cf909e110f5698d2c56abd17361d4af7ea7efbfb
lanassilva0 <lanassilvalss@gmail.com>
2025-08-27 14:56:58 -0300

    feat: implementação de array para testes

--- src/user/shared/user.service.ts
+++ src/user/shared/user.service.ts
@@ -3,20 +3,81 @@ import { User } from './user';
 
 @Injectable()
 export class UserService {
-  findById(googleId: string) {
-    // lógica para encontrar um usuário pelo googleId
-    return googleId;
+  users: User[] = [
+    {
+      googleId: 101,
+      name: 'Alice Johnson',
+      email: 'alice.johnson@example.com',
+      avatar: 'https://avatars.googleusercontent.com/u/101',
+      locale: 'en-US',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      status: true,
+    },
+    {
+      googleId: 102,
+      name: 'Bruno Silva',
+      email: 'bruno.silva@example.com',
+      avatar: 'https://avatars.googleusercontent.com/u/102',
+      locale: 'pt-BR',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      status: true,
+    },
+    {
+      googleId: 103,
+      name: 'Carlos Rodriguez',
+      email: 'carlos.rodriguez@example.com',
+      avatar: 'https://avatars.googleusercontent.com/u/103',
+      locale: 'es-ES',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      status: false,
+    },
+    {
+      googleId: 104,
+      name: 'Diana Smith',
+      email: 'diana.smith@example.com',
+      avatar: 'https://avatars.googleusercontent.com/u/104',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      status: true,
+    },
+    {
+      googleId: 105,
+      name: 'Elena Petrov',
+      email: 'elena.petrov@example.com',
+      avatar: 'https://avatars.googleusercontent.com/u/105',
+      locale: 'ru-RU',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      status: false,
+    },
+  ];
+
+  findById(id: number) {
+    const userFind = this.users.find((user) => user.googleId === id);
+
+    return userFind;
   }
 
   findByEmail(email: string) {
-    // lógica para encontrar um usuário pelo email
+    return this.users.find((user) => user.email === email);
   }
 
-  create (userData: User) {
-    // lógica para criar um novo usuário
+  create(userData: User) {
+    console.log(userData);
   }
 
   findOrCreate(userData: User) {
-    // lógica para encontrar ou criar um usuário
+    console.log(userData);
+  }
+
+  getAll() {
+    return [];
+  }
+
+  disableUser(id: number) {
+    return this.users[id];
   }
 }
