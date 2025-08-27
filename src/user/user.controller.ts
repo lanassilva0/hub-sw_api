45c23128d5b264fd12536222c7dede004be1bcb2
lanassilva0 <lanassilvalss@gmail.com>
2025-08-27 14:57:12 -0300

    fix: ajustando a tipagem do retorno

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+/* eslint-disable @typescript-eslint/require-await */
+import { Controller, Get, NotFoundException, Param, Put } from '@nestjs/common';
+import { UserService } from './shared/user.service';
+import { User } from './shared/user';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Get()
+  async getAllUsers(): Promise<User[]> {
+    return this.userService.getAll();
+  }
+
+  @Get(':id')
+  async getUserById(@Param('id') id: string): Promise<User> {
+    const user = this.userService.findById(parseInt(id, 10));
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    return user;
+  }
+
+  @Put(':id')
+  async updateUser(@Param('id') id: string): Promise<User> {
+    return this.userService.disableUser(parseInt(id, 10));
+  }
+}
