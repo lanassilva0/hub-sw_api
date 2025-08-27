2a722f021fed4e5a57af9e456ade8f18dd4ce1c4
lanassilva0 <lanassilvalss@gmail.com>
2025-08-27 14:57:32 -0300

    feat: novo atributo para classificar o usuário

--- src/user/shared/user.ts
+++ src/user/shared/user.ts
@@ -1,9 +1,10 @@
 export class User {
-  googleId: string;
+  googleId: number;
   name: string;
   email: string;
   avatar: string;
   locale?: string;
   createdAt: Date;
   updatedAt: Date;
+  status: Boolean;
 }
