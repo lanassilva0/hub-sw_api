031cc9516eba2a8073091bb95d4307c78cbc06f2
lanassilva0 <lanassilvalss@gmail.com>
2025-08-28 21:55:43 -0300

    feat: inicialização do module da autenticação

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import googleOauthConfig from './config/google-oauth.config';
+import { AuthController } from './auth.controller';
+import { AuthService } from './shared/auth.service';
 
-@Module({})
+@Module({
+  imports: [ConfigModule.forFeature(googleOauthConfig)],
+  controllers: [AuthController],
+  providers: [AuthService],
+})
 export class AuthModule {}
