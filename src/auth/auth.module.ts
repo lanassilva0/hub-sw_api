870679468b27aadd6fb313dfc65cb402d023effe
lanassilva0 <lanassilvalss@gmail.com>
2025-08-31 13:59:21 -0300

    feat: implementação inicial do module de autenticação

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,10 +3,18 @@ import { ConfigModule } from '@nestjs/config';
 import googleOauthConfig from './config/google-oauth.config';
 import { AuthController } from './auth.controller';
 import { AuthService } from './shared/auth.service';
+import { UserService } from 'src/user/shared/user.service';
+import { User } from 'src/user/schemas/user.schema';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { GoogleStrategy } from './strategies/google.strategies';
+// import { APP_GUARD } from '@nestjs/core';
 
 @Module({
-  imports: [ConfigModule.forFeature(googleOauthConfig)],
+  imports: [
+    ConfigModule.forFeature(googleOauthConfig),
+    TypeOrmModule.forFeature([User]),
+  ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, GoogleStrategy, UserService],
 })
 export class AuthModule {}
