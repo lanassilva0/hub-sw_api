f4be44e6deb0d5edeb9a2a0c1d167789dbe3fd9c
lanassilva0 <lanassilvalss@gmail.com>
2025-08-31 19:35:22 -0300

    feat: implementação do strategy do JWT

---
+++ src/auth/strategies/jwt.strategies.ts
@@ -0,0 +1,30 @@
+import type { ConfigType } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import jwtConfig from '../config/jwt.config';
+import { AuthJwtPayload } from '../types/auth-jwtPayload';
+import { Inject, Injectable } from '@nestjs/common';
+import { AuthService } from '../shared/auth.service';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    @Inject(jwtConfig.KEY)
+    private jwtConfiguration: ConfigType<typeof jwtConfig>,
+    private authService: AuthService,
+  ) {
+    if (!jwtConfiguration.secret) {
+      throw new Error('JWT secret is not defined in configuration');
+    }
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: jwtConfiguration.secret,
+      ignoreExpiration: false,
+    });
+  }
+
+  validate(payload: AuthJwtPayload) {
+    const userId = payload.sub;
+    return this.authService.validateJwtUser(String(userId));
+  }
+}
