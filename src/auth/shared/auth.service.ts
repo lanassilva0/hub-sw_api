17023bac1a0ec5e82bf333d3db2a69247604a85b
lanassilva0 <lanassilvalss@gmail.com>
2025-08-31 14:00:13 -0300

    feat: implementação inicial do service de autenticação

--- src/auth/shared/auth.service.ts
+++ src/auth/shared/auth.service.ts
@@ -1,4 +1,90 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { compare } from 'bcrypt';
+import { UserService } from 'src/user/shared/user.service';
+import { ConfigType } from '@nestjs/config';
+import * as argon2 from 'argon2';
+import { CurrentUser } from './types/current-user';
+import { CreateUserDto } from 'src/user/dto/create-user.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private userService: UserService) {}
+
+  async validateUser(email: string, password: string) {
+    const user = await this.userService.findByEmail(email);
+    if (!user) throw new UnauthorizedException('User not found!');
+    const isPasswordMatch = await compare(password, user.password);
+    if (!isPasswordMatch)
+      throw new UnauthorizedException('Invalid credentials');
+
+    return { id: user.id };
+  }
+
+  async login(userId: number) {
+    // const payload: AuthJwtPayload = { sub: userId };
+    // const token = this.jwtService.sign(payload);
+    // const refreshToken = this.jwtService.sign(payload, this.refreshTokenConfig);
+    const { accessToken, refreshToken } = await this.generateTokens(userId);
+    const hashedRefreshToken = await argon2.hash(refreshToken);
+    await this.userService.updateHashedRefreshToken(userId, hashedRefreshToken);
+    return {
+      id: userId,
+      accessToken,
+      refreshToken,
+    };
+  }
+  //   async generateTokens(userId: number) {
+  //     const payload: AuthJwtPayload = { sub: userId };
+  //     const [accessToken, refreshToken] = await Promise.all([
+  //       this.jwtService.signAsync(payload),
+  //       this.jwtService.signAsync(payload, this.refreshTokenConfig),
+  //     ]);
+  //     return {
+  //       accessToken,
+  //       refreshToken,
+  //     };
+  //   }
+
+  async refreshToken(userId: number) {
+    const { accessToken, refreshToken } = await this.generateTokens(userId);
+    const hashedRefreshToken = await argon2.hash(refreshToken);
+    await this.userService.updateHashedRefreshToken(userId, hashedRefreshToken);
+    return {
+      id: userId,
+      accessToken,
+      refreshToken,
+    };
+  }
+
+  async validateRefreshToken(userId: number, refreshToken: string) {
+    const user = await this.userService.findOne(userId);
+    if (!user || !user.hashedRefreshToken)
+      throw new UnauthorizedException('Invalid Refresh Token');
+
+    const refreshTokenMatches = await argon2.verify(
+      user.hashedRefreshToken,
+      refreshToken,
+    );
+    if (!refreshTokenMatches)
+      throw new UnauthorizedException('Invalid Refresh Token');
+
+    return { id: userId };
+  }
+
+  async signOut(userId: number) {
+    await this.userService.updateHashedRefreshToken(userId, null);
+  }
+
+  async validateJwtUser(userId: number) {
+    const user = await this.userService.findOne(userId);
+    if (!user) throw new UnauthorizedException('User not found!');
+    const currentUser: CurrentUser = { id: user.id, role: user.role };
+    return currentUser;
+  }
+
+  async validateGoogleUser(googleUser: CreateUserDto) {
+    const user = await this.userService.findByEmail(googleUser.email);
+    if (user) return user;
+    return await this.userService.create(googleUser);
+  }
+}
