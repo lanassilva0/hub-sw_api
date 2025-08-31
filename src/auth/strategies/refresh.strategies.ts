f01e132793dd5f90615aebeb52a2da5556e56e61
lanassilva0 <lanassilvalss@gmail.com>
2025-08-31 19:40:07 -0300

    feat: implementação do strategies do refresh

---
+++ src/auth/strategies/refresh.strategies.ts
@@ -0,0 +1,43 @@
+import type { ConfigType } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy, StrategyOptionsWithRequest } from 'passport-jwt';
+// import jwtConfig from '../config/jwt.config';
+import { AuthJwtPayload } from '../types/auth-jwtPayload';
+import { Inject, Injectable } from '@nestjs/common';
+import refreshJwtConfig from '../config/refresh-jwt.config';
+import { Request } from 'express';
+import { AuthService } from '../shared/auth.service';
+
+@Injectable()
+export class RefreshJwtStrategy extends PassportStrategy(
+  Strategy,
+  'refresh-jwt',
+) {
+  constructor(
+    @Inject(refreshJwtConfig.KEY)
+    private refrshJwtConfiguration: ConfigType<typeof refreshJwtConfig>,
+    private authService: AuthService,
+  ) {
+    if (!refrshJwtConfiguration.secret) {
+      throw new Error('Refresh JWT secret is not defined');
+    }
+    const options: StrategyOptionsWithRequest = {
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: refrshJwtConfiguration.secret,
+      ignoreExpiration: false,
+      passReqToCallback: true,
+    };
+    super(options);
+  }
+
+  // authorization: Bearer sldfk;lsdkf'lskald'sdkf;sdl
+
+  validate(req: Request, payload: AuthJwtPayload) {
+    const authHeader = req.get('authorization');
+    const refreshToken = authHeader
+      ? authHeader.replace('Bearer', '').trim()
+      : '';
+    const userId = String(payload.sub);
+    return this.authService.validateRefreshToken(userId, refreshToken);
+  }
+}
