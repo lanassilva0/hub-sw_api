11e1c8d34f8c4684382b97e647cf0fb1cb8ea2ac
lanassilva0 <lanassilvalss@gmail.com>
2025-08-31 16:46:45 -0300

    fix: ajustando a config

--- src/auth/config/refresh-jwt.config.ts
+++ src/auth/config/refresh-jwt.config.ts
@@ -1,5 +1,11 @@
-import { Injectable } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
+import { registerAs } from '@nestjs/config';
+import { JwtSignOptions } from '@nestjs/jwt';
 
-@Injectable()
-export class RefreshAuthGuard extends AuthGuard('refresh-jwt') {}
+export default registerAs(
+  'refresh-jwt',
+  (): JwtSignOptions => ({
+    secret: process.env.REFRESH_JWT_SECRET,
+
+    expiresIn: process.env.REFRESH_JWT_EXPIRE_IN,
+  }),
+);
