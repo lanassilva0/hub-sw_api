8e538cd413fe8234f87dcfadb8af109c70f7f772
lanassilva0 <lanassilvalss@gmail.com>
2025-08-31 13:59:44 -0300

    feat: implementação inicial do controller de autenticação

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,53 @@
-import { Controller } from '@nestjs/common';
-
+import {
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Req,
+  Request,
+  Res,
+  UseGuards,
+} from '@nestjs/common';
+import { AuthService } from './shared/auth.service';
+// import { AuthGuard } from '@nestjs/passport';
+import { LocalAuthGuard } from './guards/local-auth/local-auth.guard';
+import { RefreshAuthGuard } from './guards/refresh-auth/refresh-auth.guard';
+import { GoogleAuthGuard } from './guards/google-auth/google-auth.guard';
+import { Public } from '@mui/icons-material';
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+  @Public()
+  @HttpCode(HttpStatus.OK)
+  @UseGuards(LocalAuthGuard)
+  @Post('login')
+  async login(@Request() req) {
+    return this.authService.login(req.user.id);
+  }
+
+  @UseGuards(RefreshAuthGuard)
+  @Post('refresh')
+  refreshToken(@Req() req) {
+    return this.authService.refreshToken(req.user.id);
+  }
+
+  //   @UseGuards(JwtAuthGuard)
+  //   @Post('signout')
+  //   signOut(@Req() req) {
+  //     this.authService.signOut(req.user.id);
+  //   }
+
+  @Public()
+  @UseGuards(GoogleAuthGuard)
+  @Get('google/login')
+  googleLogin() {}
+
+  @Public()
+  @UseGuards(GoogleAuthGuard)
+  @Get('google/callback')
+  async googleCallback(@Req() req, @Res() res) {
+    const response = await this.authService.login(req.user.id);
+    res.redirect(`http://localhost:5173?token=${response.accessToken}`);
+  }
+}
