8fa0ab01ab43bd49e924569686d4fd40c185894a
lanassilva0 <lanassilvalss@gmail.com>
2025-08-31 19:55:08 -0300

    fix: ajustando nome de variáveis

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -8,19 +8,19 @@ import {
   Body,
 } from '@nestjs/common';
 import { UserService } from './shared/user.service';
-import { User } from './shared/user';
+import { CurrentUser } from './shared/user';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Get()
-  async getAllUsers(): Promise<User[]> {
+  async getAllUsers(): Promise<CurrentUser[]> {
     return this.userService.getAll();
   }
 
   @Get(':id')
-  async getUserById(@Param('id') id: string): Promise<User> {
+  async getUserById(@Param('id') id: string): Promise<CurrentUser> {
     const user = await this.userService.findById(id);
     if (!user) {
       throw new NotFoundException('User not found');
@@ -29,7 +29,7 @@ export class UserController {
   }
 
   @Put(':id')
-  async updateUser(@Param('id') id: string): Promise<User> {
+  async updateUser(@Param('id') id: string): Promise<CurrentUser> {
     const user = await this.userService.disableUser(id);
     if (!user) {
       throw new NotFoundException('User not found');
@@ -38,7 +38,7 @@ export class UserController {
   }
 
   @Post()
-  async createUser(@Body() userData: User): Promise<User> {
+  async createUser(@Body() userData: CurrentUser): Promise<CurrentUser> {
     return this.userService.create(userData);
   }
 }
