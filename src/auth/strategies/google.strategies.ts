3e0cf2f3672be6adc3a21dd395b047d6a79a3832
lanassilva0 <lanassilvalss@gmail.com>
2025-09-30 08:55:19 -0300

    fix: reformulando alterando os tipos de usuario com o intuito de correção de erros

--- src/auth/strategies/google.strategies.ts
+++ src/auth/strategies/google.strategies.ts
@@ -1,9 +1,13 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import { Model } from 'mongoose';
 import { Inject, Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy, VerifyCallback } from 'passport-google-oauth20';
 import googleOauthConfig from '../config/google-oauth.config';
 import type { ConfigType } from '@nestjs/config';
 import { AuthService } from '../auth.service';
+import { UserModel } from '../../user/user.model';
 
 @Injectable()
 export class GoogleStrategy extends PassportStrategy(Strategy) {
@@ -11,28 +15,48 @@ export class GoogleStrategy extends PassportStrategy(Strategy) {
     @Inject(googleOauthConfig.KEY)
     private googleConfiguration: ConfigType<typeof googleOauthConfig>,
     private authService: AuthService,
+    private userModel: Model<UserModel>,
   ) {
+    if (
+      !googleConfiguration.clientID ||
+      !googleConfiguration.clientSecret ||
+      !googleConfiguration.callbackURL
+    ) {
+      throw new Error(
+        'Google OAuth configuration is missing required environment variables.',
+      );
+    }
+
     super({
-      clientID: googleConfiguration?.clinetID,
+      clientID: googleConfiguration?.clientID,
       clientSecret: googleConfiguration?.clientSecret,
       callbackURL: googleConfiguration?.callbackURL,
       scope: ['email', 'profile'],
+      passReqToCallback: true,
     });
   }
 
   async validate(
+    req: Request,
     accessToken: string,
     refreshToken: string,
     profile: any,
     done: VerifyCallback,
   ) {
     console.log({ profile });
+    if (!profile) {
+      throw new Error('No profile returned from Google');
+    }
+
     const user = await this.authService.validateGoogleUser({
-      email: profile.emails[0].value,
-      firstName: profile.name.givenName,
-      lastName: profile.name.familyName,
-      avatarUrl: profile.photos[0].value,
+      googleId: profile?.id,
+      email: profile?.email,
+      name: profile?.name,
+      avatar: profile?.picture,
       password: '',
+      status: true,
+      locale: profile?.locale,
+      hashedRefreshToken: refreshToken,
     });
     done(null, user);
     return user;
